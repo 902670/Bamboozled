bb424349182de1e699539caa55dc02d5549bfde0
Henry65 <902670@northmen.org>
2026-02-16 15:41:11 -0500

    V1.2.3
    Working on the widget. Nothings done yet.

--- Bamboozled/src/main/java/com/example/Bamboozled/BambuWidget.kt
+++ Bamboozled/src/main/java/com/example/Bamboozled/BambuWidget.kt
@@ -1,47 +1,66 @@
 package com.example.Bamboozled
 
 import android.content.Context
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
-import androidx.glance.appwidget.CircularProgressIndicator
+import androidx.glance.ImageProvider
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.components.Scaffold
+import androidx.glance.appwidget.components.TitleBar
+import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.*
-
+import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
 
 class BambuWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
+            val printerState by PrinterDataManager.state.collectAsState()
             GlanceTheme {
-                WidgetContent()
+
+
+
+                    //Widget Content
+
+                    Row(
+                        modifier = GlanceModifier
+                            .fillMaxSize()
+                    ) {
+                        Box(
+                            modifier = GlanceModifier
+                                .background(GlanceTheme.colors.secondaryContainer)
+                                .cornerRadius(20.dp)
+                                .fillMaxSize()
+                                .defaultWeight()
+
+                        ) {
+
+
+                        }
+                        Spacer(modifier = GlanceModifier.width(8.dp))
+                        Box(
+                            modifier = GlanceModifier
+                                .background(GlanceTheme.colors.tertiaryContainer)
+                                .cornerRadius(20.dp)
+                                .fillMaxSize()
+                                .defaultWeight()
+                        ) {
+
+                        }
+                    }
+                }
             }
         }
     }
 
-    @Composable
-    fun WidgetContent() {
-        Box(
-            modifier = GlanceModifier.fillMaxSize().background(GlanceTheme.colors.onPrimary)
-        ) {
-            Row (GlanceModifier.fillMaxSize()) {
-                CircularProgressIndicator(
-                    GlanceModifier.fillMaxSize()
-
-
-                )
-                CircularProgressIndicator(
-                    GlanceModifier.fillMaxSize()
-                )
-
-            }
-        }
-    }
-}
 
 class BambuWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = BambuWidget()
--- Bamboozled/src/main/java/com/example/Bamboozled/MainActivity.kt
+++ Bamboozled/src/main/java/com/example/Bamboozled/MainActivity.kt
@@ -52,6 +52,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
